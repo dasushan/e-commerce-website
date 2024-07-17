777679b88e161dcdc9a662e9ea24a2e5b5416f80
dasushan <53913896+dasushan@users.noreply.github.com>
2024-07-18 00:46:00 +0530

    added header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css'
+const Header = () => {
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">Home</div>
+        <div className="col">Store</div>
+        <div className="col">About</div>
+        <div className='col'>cart</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
