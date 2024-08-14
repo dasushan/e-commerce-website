ec956a642bd9d644179585fc7a7d12cb01c78ffc
dasushan <53913896+dasushan@users.noreply.github.com>
2024-08-14 13:55:16 +0530

    adding product page

---
+++ src/pages/Product.js
@@ -0,0 +1,13 @@
+import Footer from '../components/Footer/Footer';
+import AvailableProduct from '../components/Products/AvailableProducts/AvailableProduct';
+
+const Product = () => {
+  return (
+    <>
+      <AvailableProduct />
+      <Footer />
+    </>
+  );
+};
+
+export default Product;
