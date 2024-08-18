86367685415e6838d55c4bde070d072e01e328b1
dasushan <53913896+dasushan@users.noreply.github.com>
2024-08-18 12:44:21 +0530

    persisting user-emailId--localStorage

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 
 const AuthContext = React.createContext({
     token: '',
+    emailId: '',
     isLoggedIn: false,
     login:(token) => {},
     logout: () => {}
@@ -9,21 +10,30 @@ const AuthContext = React.createContext({
 
 export const AuthContextProvider = (props) => {
     const initialToken = localStorage.getItem('token')
+    const initialEmailId = localStorage.getItem('emailId')
     const [token, setToken] = useState(initialToken);
+    const [emailId, setEmailId] = useState(initialEmailId)
 
     const userIsLoggedIn = !!token;
     console.log(userIsLoggedIn);
-    const loginHandler = (token) => {
-        setToken(token)
-        localStorage.setItem('token', token)
+    console.log(token)
+    console.log(emailId)
+
+    const loginHandler = (data) => {
+        setToken(data.idToken)
+        setEmailId(data.email)
+        localStorage.setItem('token', data.idToken)
+        localStorage.setItem('emailId', data.email)
     }
 
     const logoutHandler = () => {
         setToken(null)
         localStorage.removeItem('token')
+        localStorage.removeItem('emailId')
     }
     const contextValue = {
         token: token,
+        emailId: emailId,
         isLoggedIn: userIsLoggedIn,
         login: loginHandler,
         logout: logoutHandler,
